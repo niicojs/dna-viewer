f07fb0e59afa8d011909c8c3d0874d42dd4d9ff0
niico <5653050+niicojs@users.noreply.github.com>
2026-04-06 11:01:10 +0200

    init

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import tailwindcss from '@tailwindcss/vite';
+import { devtools } from '@tanstack/devtools-vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import viteReact from '@vitejs/plugin-react';
+import { nitro } from 'nitro/vite';
+import { defineConfig } from 'vite-plus';
+
+const config = defineConfig({
+  fmt: { singleQuote: true, printWidth: 120, sortImports: true, sortTailwindcss: true },
+  lint: { options: { typeAware: true, typeCheck: true } },
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    nitro({ rollupConfig: { external: [/^@sentry\//] } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+});
+
+export default config;
